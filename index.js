629e109f8239aac4c81c6b3b6f9fea90b806bb95
Francisco Ambrosioni <ambrosionifrancisco@gmail.com>
2023-03-09 17:09:09 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+let descripcionEolicaA = document.getElementById('descripcionEolicaA');
+let energiaEolicaA = document.getElementById('energiaEolicaA');
+let presupEolicaA = document.getElementById('presupEolicaA');
+let tiempoEolicaA = document.getElementById('tiempoEolicaA');
+
+let descripcionEolicaB = document.getElementById('descripcionEolicaB');
+let energiaEolicaB = document.getElementById('energiaEolicaB');
+let presupEolicaB = document.getElementById('presupEolicaB');
+let tiempoEolicaB = document.getElementById('tiempoEolicaB');
+
+let descripcionHidroA = document.getElementById('descripcionHidroA');
+let energiaHidroA = document.getElementById('energiaHidroA');
+let presupHidroA = document.getElementById('presupHidroA');
+let tiempoHidroA = document.getElementById('tiempoHidroA');
+
+let descripcionHidroB = document.getElementById('descripcionHidroB');
+let energiaHidroB = document.getElementById('energiaHidroB');
+let presupHidroB = document.getElementById('presupHidroB');
+let tiempoHidroB = document.getElementById('tiempoHidroB');
+
+let descripcionSolarA = document.getElementById('descripcionSolarA');
+let energiaSolarA = document.getElementById('energiaSolarA');
+let presupSolarA = document.getElementById('presupSolarA');
+let tiempoSolarA = document.getElementById('tiempoSolarA');
+
+let descripcionSolarB = document.getElementById('descripcionSolarA');
+let energiaSolarB = document.getElementById('energiaSolarB');
+let presupSolarB = document.getElementById('presupSolarB');
+let tiempoSolarB = document.getElementById('tiempoSolarB');
+
+
+console.log('descriocion' + descripcionEolicaA);
